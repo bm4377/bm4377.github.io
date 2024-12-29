d51aaf8bbdf6225b535f91e0a8ef9a9e34a5153e
bm4377 <153153439+bm4377@users.noreply.github.com>
2024-12-30 00:18:11 +0630

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-const player = new Player('#player-container', {
-    file: 'https://hello-world-silent-sun-f8f9.koyehtaeoo.workers.dev/0:/ballerina_2025_official_trailer.mp4', // Replace with the actual filename
-    autoplay: false,
-    muted: false,
-    controls: true,
-    // ... other options
-});
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const videoUrl = urlParams.get('videoUrl');
+
+if (videoUrl) {
+    const player = new Player('#player-container', {
+        file: videoUrl,
+        autoplay: false,
+        muted: false,
+        controls: true,
+        // ... other options
+    });
+} else {
+    console.error("Missing video URL parameter");
+}
